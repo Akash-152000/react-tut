00adf8c6c09921e382ada712aabfaae4fb1cc418
Akash-152000 <akash152000akash@gmail.com>
2024-01-04 20:47:42 +0530

    Configured Redux with react

--- appwrite_project/src/App.jsx
+++ appwrite_project/src/App.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
 import config from './config/config'
+import authService from './appwrite/appwrite_services'
+import { login, logout } from './Store/authSlice'
+import { Footer, Header } from './components'
 
 function App() {
 
-  return (
-    <>
-    {console.log(config.appwriteUrl)}
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+
+
+  },[])
+
+  return !loading ?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className= 'w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ): (null)
 }
 
 export default App
